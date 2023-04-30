8c33e747387fe5df72d129905f69578cbd3a5e89
Mrbt0907 <Mrbt0907@MrBTPC1>
2023-04-29 18:27:55 -0700

    Disabled grab optimizations by default for now as it's broken

--- src/main/java/net/mrbt0907/weather2/config/ConfigGrab.java
+++ src/main/java/net/mrbt0907/weather2/config/ConfigGrab.java
@@ -10,7 +10,7 @@ import java.io.File;
 public class ConfigGrab implements IConfigCategory
 {
 	@ConfigComment("Should weather2 use the old getters and setters for blocks?")
-	public static boolean disableGrabOptimizations = false;
+	public static boolean disableGrabOptimizations = true;
 	@ConfigComment("Should storms grab players?")
 	public static boolean grab_players = true;
 	@ConfigComment("Should storms grab mobs?")
